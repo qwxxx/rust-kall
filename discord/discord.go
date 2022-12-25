88501507d325d8ad57319811ada45d5ce9c352ba
Ripper <7309333@gmail.com>
2022-12-26 00:28:18 +0300

    Add robot to message

--- discord/discord.go
+++ discord/discord.go
@@ -50,7 +50,7 @@ func (c *Discord) SendImportant() {
 	currentDateMsk := time.Now().UTC().Add(time.Hour * 3)
 	if currentDateMsk.Day() != lastReportDateMsk.Day() && currentDateMsk.Hour() >= 13 && int(time.Now().Weekday()) == 4 {
 		lastReportDateMsk = currentDateMsk
-		c.session.ChannelMessageSend(ImportantChannelID, "@everyone\nНапоминаю, вы автоматически зарегистрировались через билет 95$ в турнире (кто по еженедельном ЛБ на эту ступеньку попал)\nЕсли не планируете играть именно этот турнир - отрегистрируйтесь")
+		c.session.ChannelMessageSend(ImportantChannelID, ":robot: @everyone\nНапоминаю, вы автоматически зарегистрировались через билет 95$ в турнире (кто по еженедельном ЛБ на эту ступеньку попал)\nЕсли не планируете играть именно этот турнир - отрегистрируйтесь")
 	}
 }
 func (c *Discord) SendTournamentInfo(response global.CalculateTournamentResponse) string {
